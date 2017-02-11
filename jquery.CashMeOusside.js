88b881af75329445ff6e32a0bde6a1a7bb3760e6
Jason <jason_e66@msn.com>
2017-02-11 16:23:44 -0600

    Update jquery.CashMeOusside.js
    Added preload tag to audio

--- jquery.CashMeOusside.js
+++ jquery.CashMeOusside.js
@@ -6,53 +6,53 @@
  * http://www.opensource.org/licenses/mit-license.php
 */
 ;(function ($) {
-    function CashMeOusside (element, options) {
+    	function CashMeOusside (element, options) {
 		this.element = element;
-        this.settings = $.extend({}, $.fn.CashMeOusside.defaults, options);
-        this.init();
+        	this.settings = $.extend({}, $.fn.CashMeOusside.defaults, options);
+        	this.init();
 	}
 
-    $.extend(CashMeOusside.prototype, {
+    	$.extend(CashMeOusside.prototype, {
 		init: function() {
  			$("body").append('<img id="cash-me-ousside" src="'+ this.settings.image +'" alt="howbowdah" />');
 			$('#cash-me-ousside').css('position', 'fixed');
 			$('#cash-me-ousside').css('left', '50%');
-            $('#cash-me-ousside').css('margin-left', '-250px');
+            		$('#cash-me-ousside').css('margin-left', '-250px');
 			$('#cash-me-ousside').css('bottom', '-500px');
 			if(this.settings.hasSound){
-				$("body").append('<audio id="cash-me-ousside-audio"><source src="'+ this.settings.sound +'" type="audio/mpeg"></source></audio>');
+				$("body").append('<audio id="cash-me-ousside-audio" preload="auto"><source src="'+ this.settings.sound +'" type="audio/mpeg"></source></audio>');
  			}
 		},
 		show: function() {
-            var audio = document.getElementById('cash-me-ousside-audio');
+            		var audio = document.getElementById('cash-me-ousside-audio');
 			audio.play();
-            $('#cash-me-ousside').animate({bottom:'0px'},500,function(){
-    	        setTimeout(function(){hideMe();},1000);
-            });
+            		$('#cash-me-ousside').animate({bottom:'0px'},500,function(){
+    	        		setTimeout(function(){hideMe();},1000);
+            		});
 		}
 	});
 
-    var hideMe = function() {
-        $('#cash-me-ousside').animate({bottom:'-500px'},500);
-    };
+    	var hideMe = function() {
+        	$('#cash-me-ousside').animate({bottom:'-500px'},500);
+    	};
 
-    // A really lightweight plugin wrapper around the constructor,
+    	// A really lightweight plugin wrapper around the constructor,
 	// preventing against multiple instantiations
 	$.fn.CashMeOusside = function(options) {
 		var plugin = this.data('plugin_cashmeousside');
 
-        // has plugin instantiated ?
-        if (plugin instanceof Plugin) {
-            //do nothing
-        } else {
-            plugin = new CashMeOusside(this, options);
-            this.data('plugin_cashmeousside', plugin);
-        }
+        	// has plugin instantiated ?
+        	if (plugin instanceof Plugin) {
+            		//do nothing
+        	} else {
+            		plugin = new CashMeOusside(this, options);
+            		this.data('plugin_cashmeousside', plugin);
+        	}
 
-        return plugin;
+        	return plugin;
 	};
 
-    $.fn.CashMeOusside.defaults = {
+    	$.fn.CashMeOusside.defaults = {
  		hasSound: true,
  		image: 'cashmeousside.png',
  		sound: 'cashmeoussidehowbowdah.mp3'
